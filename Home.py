a326c0f34229d5c83e34f7ff63854a282cea9df5
Ankush7-cloud <ankushchowdhury01@gmail.com>
2025-06-26 11:25:21 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 def app():
-    st.title("🏠 Home Page")
-    st.write("Welcome to the *Device Management System*.")
+    st.image("isi.jpg",width=150)
+    st.title("IDEAS - Institute of Data Engineering, Analytics and Science Foundation")
+    st.subheader("Welcome to the platform!")
+    st.markdown("""
+    ### 💡 IDEAS - Institute of Data Engineering, Analytics and Science Foundation
+Technology Innovation Hub @ Indian Statistical Institute, Kolkata
+Funded  by (NM-ICPS), Department of Science and Technology (DST), Government of India
+""")
